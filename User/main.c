8200c936b39fc3324b4082aa5d7e43b5afa05e7d
Liu Pengguo <2024213141@stu.cqupt.edu.cn>
2026-01-03 14:15:59 +0800

    V1.5 蜂鸣器已响，周末开始

--- User/main.c
+++ User/main.c
@@ -1,4 +1,8 @@
 #include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+
+#define BUZZER_ON GPIO_ResetBits(GPIOB, GPIO_Pin_12)
+#define BUZZER_OFF GPIO_SetBits(GPIOB, GPIO_Pin_12)
 
 int main(void)
 {
@@ -10,20 +14,34 @@ int main(void)
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
 
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
 	GPIO_Init(GPIOB,&GPIO_InitStructure);
 	
+	GPIO_SetBits(GPIOC, GPIO_Pin_12);
+	BUZZER_OFF;
+	
 	while(1)
 	{
 		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)
 		{
-			GPIO_SetBits(GPIOC, GPIO_Pin_13);
+			Delay_ms(20);
+			if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)
+			{
+				GPIO_SetBits(GPIOC, GPIO_Pin_13);
+				BUZZER_OFF;
+			}
 		}
 		else 
 		{
 			GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+			BUZZER_ON;
 		}
 	}
 }		

c2e4e69e9de29c5af253bcc9b7f99f95064bacb9
Liu Pengguo <2024213141@stu.cqupt.edu.cn>
2026-01-03 11:47:28 +0800

    V1.0 完成：按键控制LED

--- User/main.c
+++ User/main.c
@@ -4,25 +4,26 @@ int main(void)
 {
 	GPIO_InitTypeDef GPIO_InitStructure;	 
 	
-	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOB,ENABLE);
 	
 	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOC,&GPIO_InitStructure);
 
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_Init(GPIOB,&GPIO_InitStructure);
+	
 	while(1)
 	{
-		GPIO_ResetBits(GPIOC,GPIO_Pin_13);
-		
-		// 笨蛋延时 (随便空跑一会)
-        int i; // 这一行如果报错，也要提到 while(1) 外面去定义
-        for(i=0; i<1000000; i++);
-        
-        // 灭灯
-        GPIO_SetBits(GPIOC, GPIO_Pin_13);
-        
-        // 再延时
-        for(i=0; i<1000000; i++);
+		if (GPIO_ReadInputDataBit(GPIOB, GPIO_Pin_1) == 0)
+		{
+			GPIO_SetBits(GPIOC, GPIO_Pin_13);
+		}
+		else 
+		{
+			GPIO_ResetBits(GPIOC, GPIO_Pin_13);
+		}
 	}
 }		
